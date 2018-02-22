ce6ace2ede74fea2250317b260fa9558b394a455
Hasan ÖKTEN <hokten@gmail.com>
2018-02-22 15:37:18 +0200

    DavetiyeCevap modeli oluşturuldu.

---
+++ Davetiye/Models/DavetiyeCevap.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Davetiye.Models
+{
+    public class DavetiyeCevap
+    {
+        public string AdSoyad { get; set; }
+        public string Email { get; set; }
+        public string Telefon { get; set; }
+        public bool? KatilimDurumu { get; set; }
+
+    }
+}
